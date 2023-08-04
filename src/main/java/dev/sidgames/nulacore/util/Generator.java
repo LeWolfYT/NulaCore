f39e226e0d179597b66d6b70fb4c9a6aab4748b0
Sid <saytleader@outlook.com>
2023-08-04 18:51:35 +0530

    fix some issues

--- src/main/java/dev/sidgames/nulacore/util/Generator.java
+++ src/main/java/dev/sidgames/nulacore/util/Generator.java
@@ -7,6 +7,9 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Generator {
     public static Item generateItem(Item item, Identifier id) {
         return Registry.register(Registry.ITEM, id, item);
@@ -24,21 +27,21 @@ public class Generator {
         return Registry.register(Registry.ITEM, id, new BlockItem(block, settings));
     }
 
-    public static List<Object> generateFromList(List<Object> items, List<Identifier> ids, List<Item.Settings> settings) {
-        List<Object> Generated = new ArrayList();
-        if (not (items.size() == ids.size() == settings.size())) {
+    public static <T> List<T> generateFromList(List<T> items, List<Identifier> ids, List<Item.Settings> settings) {
+        List<Object> Generated = new ArrayList<>();
+        if (items.size() != ids.size() && ids.size() != settings.size()) {
             throw new IllegalStateException("Lists are not of the same size.");
         }
         for (int i = 0; i < items.size(); i++) {
-            if (items[i] instanceof Block) {
-                Generated.add(generateBlock(items[i], ids[i]));
-                Generated.add(generateBlockItem(items[i], ids[i], settings[i]));
-            } else if (items[i] instanceof Item) {
-                Generated.add(generateItem(items[i], ids[i]));
+            if (items.get(i) instanceof Block) {
+                Generated.add(generateBlock((Block) items.get(i), ids.get(i)));
+                Generated.add(generateBlockItem((Block) items.get(i), ids.get(i), settings.get(i)));
+            } else if (items.get(i) instanceof Item) {
+                Generated.add(generateItem((Item) items.get(i), ids.get(i)));
             } else {
                 throw new IllegalArgumentException("All objects in list must be either Block or Item.");
             }
         }
-        return Generated;
+        return (List<T>) Generated;
     }
 }
